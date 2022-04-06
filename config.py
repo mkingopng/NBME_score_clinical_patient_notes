54fdbd25c56ee655d663c09fd8e4102eb8571c11
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2022-04-06 11:20:29 +1000

    had to make the same change to dtype

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """
-
+Library imports
 """
-# libraries
+from wandb_creds import *
 import os
 import gc
 import re
@@ -17,9 +17,6 @@ import random
 import joblib
 import itertools
 import warnings
-import wandb
-
-from IPython.core.display_functions import display
 import scipy as sp
 import numpy as np
 import pandas as pd
@@ -36,53 +33,57 @@ import tokenizers
 import transformers
 from transformers import AutoTokenizer, AutoModel, AutoConfig
 from transformers import get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup
-from transformers import AutoModel, DistilBertTokenizerFast
+from DeBERTa import deberta
 
-# constants
-MODEL = AutoModel.from_pretrained('distilbert-base-uncased')
-TOKENIZER = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-CONFIG = AutoConfig.from_pretrained('distilbert-base-uncased')
-OUTPUT_DIR = 'output_dir'
+"""
+environmental variables
+"""
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 SEED = 42
-DATA_DIR = 'data'
-TRAIN = pd.read_csv(os.path.join(DATA_DIR, 'train.csv'))
-FEATURES = pd.read_csv(os.path.join(DATA_DIR, 'features.csv'))
-TEST = pd.read_csv(os.path.join(DATA_DIR, 'test.csv'))
-PATIENT_NOTES = pd.read_csv(os.path.join(DATA_DIR, 'patient_notes.csv'))
+DATA_DIR = 'kaggle/input/nbme-score-clinical-patient-notes'
+warnings.filterwarnings("ignore")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+OUTPUT_DIR = 'experiment_21_'   # iterate for each experiment
+
+"""
+CONFIGURATION
+"""
 
 
-# configuration
 class CONFIGURATION:
-    wandb = False
-    competition = 'NBME'
-    _wandb_kernel = 'mkingo'
-    debug = False
     apex = True
-    print_freq = 100
-    num_workers = 4
-    model = MODEL
-    tokenizer = TOKENIZER
-    config = CONFIG
-    scheduler = 'cosine'  # ['linear', 'cosine']
     batch_scheduler = True
-    num_cycles = 0.5
-    num_warmup_steps = 0
-    epochs = 5
-    encoder_lr = 2e-5  # mk: the use of encoder & decoder implies that this isn't straight NER
-    decoder_lr = 2e-5
-    min_lr = 1e-6
-    eps = 1e-6
+    batch_size = 8  # 8 is the sweet spot for testing. 4 for training
     betas = (0.9, 0.999)
-    batch_size = 4
-    fc_dropout = 0.2
-    max_len = 512
-    weight_decay = 0.01
+    competition = 'NBME'
+    decoder_lr = 5e-5
+    debug = False
+    epochs = 5  # 10 is better. Longer may be better still
+    encoder_lr = 2e-5  # try different LRs
     gradient_accumulation_steps = 1
-    max_grad_norm = 1000
-    seed = 42
-    n_fold = 5
-    trn_fold = [0]
+    min_lr = 1e-6  # try different LRs
+    eps = 1e-6  # try changing this
+    fc_dropout = 0.2  # try different
+    max_grad_norm = 1000  # try different
+    max_len = 512  # try different
+    model = 'microsoft/deberta-base'  # deberta  # fix_me
+    n_fold = 5  # 5 baseline
+    num_cycles = 0.5  # try different
+    num_warmup_steps = 0
+    num_workers = 4  # try different
+    print_freq = 100
+    weight_decay = 0.01  # try different
+    scheduler = 'cosine'  # try different scheduler ['linear', 'cosine']
+    seed = 42  # try different
+    trn_fold = [0, 1, 2, 3, 4]
     train = True
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    wandb = True
+    _wandb_kernel = ENTITY
 
 
 if CONFIGURATION.debug:
@@ -90,10 +91,27 @@ if CONFIGURATION.debug:
     CONFIGURATION.trn_fold = [0]
 
 
-# options
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-warnings.filterwarnings("ignore")
-TOKENIZERS_PARALLELISM = True
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+"""
+wandb
+"""
+
+if CONFIGURATION.wandb:
+    wandb.login(key=API_KEY)
+
+    def class2dict(f):
+        """
+
+        :param f:
+        :return:
+        """
+        return dict((name, getattr(f, name)) for name in dir(f) if not name.startswith('__'))
+
+    run = wandb.init(
+        project=CONFIGURATION.competition,
+        name=CONFIGURATION.model,
+        config=class2dict(CONFIGURATION),
+        group=CONFIGURATION.model,
+        job_type="train",
+    )
+    sweep_id = wandb.sweep(sweep_config, project="nbme_sweeps_testing")
+
--- train.py
+++ train.py
@@ -1,315 +1,58 @@
 """
+had 1 bug
+- data type error. runs fine on kaggle
+- the solution was a bit unusual for me...
+
+found a similar issue
+https://github.com/huggingface/transformers/issues/14375
+https://huggingface.co/microsoft/deberta-v3-large#fine-tuning-with-hf-transformers
+
+error message was
+Traceback (most recent call last): File "/home/noone/Documents/GitHub/NBME_score_clinical_patient_notes
+/train.py", line 24, in <module> _oof_df = train_loop(train, fold) File
+
+"/home/noone/Documents/GitHub/NBME_score_clinical_patient_notes/functions.py", line 858, in train_loop
+avg_loss = train_fn(fold, train_loader, model, criterion, optimizer, epoch, scheduler, device)
+
+"/home/noone/Documents/GitHub/NBME_score_clinical_patient_notes/functions.py", line 657, in train_fn
+scaler.scale(loss).backward()
+
+"/home/noone/anaconda3/envs/NBME/lib/python3.9/site-packages/torch/_tensor.py", line 363, in backward
+torch.autograd.backward(self, gradient, retain_graph, create_graph, inputs=inputs) File
+
+"/home/noone/anaconda3/envs/NBME/lib/python3.9/site-packages/torch/autograd/__init__.py", line 173, in backward
+Variable._execution_engine.run_backward(  # Calls into the C++ engine to run the backward pass File
+
+"/home/noone/anaconda3/envs/NBME/lib/python3.9/site-packages/torch/autograd/function.py", line 253, in apply return
+user_fn(self, *args) File "/home/noone/anaconda3/envs/NBME/lib/python3.9/site-packages/transformers/models/deberta
+/modeling_deberta.py", line 113, in backward inputGrad = _softmax_backward_data(grad_output, output, self.dim,
+output) TypeError: _softmax_backward_data(): argument 'input_dtype' (position 4) must be torch.dtype, not Tensor
+
+# fix_me: the error is here. changed File "/home/noone/anaconda3/envs/NBME/lib/python3.9/site-packages/transformers/
+   models/deberta/modeling_deberta.py", line 113, in backward inputGrad = _softmax_backward_data(grad_output, output,
+   self.dim, output) to inputGrad = _softmax_backward_data(grad_output, output, self.dim, output.dtype) and it fixed
+   the issue
 
 """
-from wandb_creds import *
 from functions import *
 
-# todo: debug the errors causing this to fail. related to config.json file
-
-print(f"train.shape: {TRAIN.shape}")
-print(TRAIN.head())
-print(f"features.shape: {FEATURES.shape}")
-print(FEATURES.head())
-print(f"patient_notes.shape: {PATIENT_NOTES.shape}")
-print(PATIENT_NOTES.head())
-
-train = TRAIN.merge(FEATURES, on=['feature_num', 'case_num'], how='left')
-train = train.merge(PATIENT_NOTES, on=['pn_num', 'case_num'], how='left')
-print(train.head())
-
-features = preprocess_features(FEATURES)
-
-# incorrect annotation
-train.loc[338, 'annotation'] = ast.literal_eval('[["father heart attack"]]')
-train.loc[338, 'location'] = ast.literal_eval('[["764 783"]]')
-
-train.loc[621, 'annotation'] = ast.literal_eval('[["for the last 2-3 months"]]')
-train.loc[621, 'location'] = ast.literal_eval('[["77 100"]]')
-
-train.loc[655, 'annotation'] = ast.literal_eval('[["no heat intolerance"], ["no cold intolerance"]]')
-train.loc[655, 'location'] = ast.literal_eval('[["285 292;301 312"], ["285 287;296 312"]]')
-
-train.loc[1262, 'annotation'] = ast.literal_eval('[["mother thyroid problem"]]')
-train.loc[1262, 'location'] = ast.literal_eval('[["551 557;565 580"]]')
-
-train.loc[1265, 'annotation'] = ast.literal_eval('[[\'felt like he was going to "pass out"\']]')
-train.loc[1265, 'location'] = ast.literal_eval('[["131 135;181 212"]]')
-
-train.loc[1396, 'annotation'] = ast.literal_eval('[["stool , with no blood"]]')
-train.loc[1396, 'location'] = ast.literal_eval('[["259 280"]]')
-
-train.loc[1591, 'annotation'] = ast.literal_eval('[["diarrhoe non blooody"]]')
-train.loc[1591, 'location'] = ast.literal_eval('[["176 184;201 212"]]')
-
-train.loc[1615, 'annotation'] = ast.literal_eval('[["diarrhea for last 2-3 days"]]')
-train.loc[1615, 'location'] = ast.literal_eval('[["249 257;271 288"]]')
-
-train.loc[1664, 'annotation'] = ast.literal_eval('[["no vaginal discharge"]]')
-train.loc[1664, 'location'] = ast.literal_eval('[["822 824;907 924"]]')
-
-train.loc[1714, 'annotation'] = ast.literal_eval('[["started about 8-10 hours ago"]]')
-train.loc[1714, 'location'] = ast.literal_eval('[["101 129"]]')
-
-train.loc[1929, 'annotation'] = ast.literal_eval('[["no blood in the stool"]]')
-train.loc[1929, 'location'] = ast.literal_eval('[["531 539;549 561"]]')
-
-train.loc[2134, 'annotation'] = ast.literal_eval('[["last sexually active 9 months ago"]]')
-train.loc[2134, 'location'] = ast.literal_eval('[["540 560;581 593"]]')
-
-train.loc[2191, 'annotation'] = ast.literal_eval('[["right lower quadrant pain"]]')
-train.loc[2191, 'location'] = ast.literal_eval('[["32 57"]]')
-
-train.loc[2553, 'annotation'] = ast.literal_eval('[["diarrhoea no blood"]]')
-train.loc[2553, 'location'] = ast.literal_eval('[["308 317;376 384"]]')
-
-train.loc[3124, 'annotation'] = ast.literal_eval('[["sweating"]]')
-train.loc[3124, 'location'] = ast.literal_eval('[["549 557"]]')
-
-train.loc[3858, 'annotation'] = ast.literal_eval('[["previously as regular"], ["previously eveyr 28-29 days"], ["previously lasting 5 days"], ["previously regular flow"]]')
-train.loc[3858, 'location'] = ast.literal_eval('[["102 123"], ["102 112;125 141"], ["102 112;143 157"], ["102 112;159 171"]]')
-
-train.loc[4373, 'annotation'] = ast.literal_eval('[["for 2 months"]]')
-train.loc[4373, 'location'] = ast.literal_eval('[["33 45"]]')
-
-train.loc[4763, 'annotation'] = ast.literal_eval('[["35 year old"]]')
-train.loc[4763, 'location'] = ast.literal_eval('[["5 16"]]')
-
-train.loc[4782, 'annotation'] = ast.literal_eval('[["darker brown stools"]]')
-train.loc[4782, 'location'] = ast.literal_eval('[["175 194"]]')
-
-train.loc[4908, 'annotation'] = ast.literal_eval('[["uncle with peptic ulcer"]]')
-train.loc[4908, 'location'] = ast.literal_eval('[["700 723"]]')
-
-train.loc[6016, 'annotation'] = ast.literal_eval('[["difficulty falling asleep"]]')
-train.loc[6016, 'location'] = ast.literal_eval('[["225 250"]]')
-
-train.loc[6192, 'annotation'] = ast.literal_eval('[["helps to take care of aging mother and in-laws"]]')
-train.loc[6192, 'location'] = ast.literal_eval('[["197 218;236 260"]]')
-
-train.loc[6380, 'annotation'] = ast.literal_eval('[["No hair changes"], ["No skin changes"], ["No GI changes"], ["No palpitations"], ["No excessive sweating"]]')
-train.loc[6380, 'location'] = ast.literal_eval('[["480 482;507 519"], ["480 482;499 503;512 519"], ["480 482;521 531"], ["480 482;533 545"], ["480 482;564 582"]]')
-
-train.loc[6562, 'annotation'] = ast.literal_eval('[["stressed due to taking care of her mother"], ["stressed due to taking care of husbands parents"]]')
-train.loc[6562, 'location'] = ast.literal_eval('[["290 320;327 337"], ["290 320;342 358"]]')
-
-train.loc[6862, 'annotation'] = ast.literal_eval('[["stressor taking care of many sick family members"]]')
-train.loc[6862, 'location'] = ast.literal_eval('[["288 296;324 363"]]')
-
-train.loc[7022, 'annotation'] = ast.literal_eval('[["heart started racing and felt numbness for the 1st time in her finger tips"]]')
-train.loc[7022, 'location'] = ast.literal_eval('[["108 182"]]')
-
-train.loc[7422, 'annotation'] = ast.literal_eval('[["first started 5 yrs"]]')
-train.loc[7422, 'location'] = ast.literal_eval('[["102 121"]]')
-
-train.loc[8876, 'annotation'] = ast.literal_eval('[["No shortness of breath"]]')
-train.loc[8876, 'location'] = ast.literal_eval('[["481 483;533 552"]]')
-
-train.loc[9027, 'annotation'] = ast.literal_eval('[["recent URI"], ["nasal stuffines, rhinorrhea, for 3-4 days"]]')
-train.loc[9027, 'location'] = ast.literal_eval('[["92 102"], ["123 164"]]')
-
-train.loc[9938, 'annotation'] = ast.literal_eval('[["irregularity with her cycles"], ["heavier bleeding"], ["changes her pad every couple hours"]]')
-train.loc[9938, 'location'] = ast.literal_eval('[["89 117"], ["122 138"], ["368 402"]]')
-
-train.loc[9973, 'annotation'] = ast.literal_eval('[["gaining 10-15 lbs"]]')
-train.loc[9973, 'location'] = ast.literal_eval('[["344 361"]]')
-
-train.loc[10513, 'annotation'] = ast.literal_eval('[["weight gain"], ["gain of 10-16lbs"]]')
-train.loc[10513, 'location'] = ast.literal_eval('[["600 611"], ["607 623"]]')
-
-train.loc[11551, 'annotation'] = ast.literal_eval('[["seeing her son knows are not real"]]')
-train.loc[11551, 'location'] = ast.literal_eval('[["386 400;443 461"]]')
-
-train.loc[11677, 'annotation'] = ast.literal_eval('[["saw him once in the kitchen after he died"]]')
-train.loc[11677, 'location'] = ast.literal_eval('[["160 201"]]')
-
-train.loc[12124, 'annotation'] = ast.literal_eval('[["tried Ambien but it didnt work"]]')
-train.loc[12124, 'location'] = ast.literal_eval('[["325 337;349 366"]]')
-
-train.loc[12279, 'annotation'] = ast.literal_eval('[["heard what she described as a party later than evening these things did not actually happen"]]')
-train.loc[12279, 'location'] = ast.literal_eval('[["405 459;488 524"]]')
-
-train.loc[12289, 'annotation'] = ast.literal_eval('[["experienced seeing her son at the kitchen table these things did not actually happen"]]')
-train.loc[12289, 'location'] = ast.literal_eval('[["353 400;488 524"]]')
-
-train.loc[13238, 'annotation'] = ast.literal_eval('[["SCRACHY THROAT"], ["RUNNY NOSE"]]')
-train.loc[13238, 'location'] = ast.literal_eval('[["293 307"], ["321 331"]]')
-
-train.loc[13297, 'annotation'] = ast.literal_eval('[["without improvement when taking tylenol"], ["without improvement when taking ibuprofen"]]')
-train.loc[13297, 'location'] = ast.literal_eval('[["182 221"], ["182 213;225 234"]]')
-
-train.loc[13299, 'annotation'] = ast.literal_eval('[["yesterday"], ["yesterday"]]')
-train.loc[13299, 'location'] = ast.literal_eval('[["79 88"], ["409 418"]]')
-
-train.loc[13845, 'annotation'] = ast.literal_eval('[["headache global"], ["headache throughout her head"]]')
-train.loc[13845, 'location'] = ast.literal_eval('[["86 94;230 236"], ["86 94;237 256"]]')
-
-train.loc[14083, 'annotation'] = ast.literal_eval('[["headache generalized in her head"]]')
-train.loc[14083, 'location'] = ast.literal_eval('[["56 64;156 179"]]')
-
-train['annotation_length'] = train['annotation'].apply(len)
-print(train['annotation_length'].value_counts())
-
-Fold = GroupKFold(n_splits=CONFIGURATION.n_fold)
-groups = train['pn_num'].values
-for n, (train_index, val_index) in enumerate(Fold.split(train, train['location'], groups)):
-    train.loc[val_index, 'fold'] = int(n)
-train['fold'] = train['fold'].astype(int)
-print(train.groupby('fold').size())
-
-if CONFIGURATION.debug:
-    display(train.groupby('fold').size())
-    train = train.sample(n=1000, random_state=0).reset_index(drop=True)
-    display(train.groupby('fold').size())
-
-for text_col in ['pn_history']:
-    pn_history_lengths = []
-    tk0 = tqdm(PATIENT_NOTES[text_col].fillna("").values, total=len(PATIENT_NOTES))
-    for text in tk0:
-        length = len(CONFIGURATION.tokenizer(text, add_special_tokens=False)['input_ids'])
-        pn_history_lengths.append(length)
-    LOGGER.info(f'{text_col} max(lengths): {max(pn_history_lengths)}')
-
-for text_col in ['feature_text']:
-    features_lengths = []
-    tk0 = tqdm(features[text_col].fillna("").values, total=len(features))
-    for text in tk0:
-        length = len(CONFIGURATION.tokenizer(text, add_special_tokens=False)['input_ids'])
-        features_lengths.append(length)
-    LOGGER.info(f'{text_col} max(lengths): {max(features_lengths)}')
-
-CONFIGURATION.max_len = max(pn_history_lengths) + max(features_lengths) + 3 # cls & sep & sep
-LOGGER.info(f"max_len: {CONFIGURATION.max_len}")
-
-
-# ====================================================
-# TRAIN loop
-# ====================================================
-def train_loop(folds, fold):
-    LOGGER.info(f"========== fold: {fold} training ==========")
-
-    # ====================================================
-    # loader
-    # ====================================================
-    train_folds = folds[folds['fold'] != fold].reset_index(drop=True)
-    valid_folds = folds[folds['fold'] == fold].reset_index(drop=True)
-    valid_texts = valid_folds['pn_history'].values
-    valid_labels = create_labels_for_scoring(valid_folds)
-
-    train_dataset = TrainDataset(CONFIGURATION, train_folds)
-    valid_dataset = TrainDataset(CONFIGURATION, valid_folds)
-
-    train_loader = DataLoader(train_dataset,
-                              batch_size=CONFIGURATION.batch_size,
-                              shuffle=True,
-                              num_workers=CONFIGURATION.num_workers, pin_memory=True, drop_last=True)
-    valid_loader = DataLoader(valid_dataset,
-                              batch_size=CONFIGURATION.batch_size,
-                              shuffle=False,
-                              num_workers=CONFIGURATION.num_workers, pin_memory=True, drop_last=False)
-
-    # ====================================================
-    # model & optimizer
-    # ====================================================
-    model = CustomModel(CONFIGURATION, config_path=None, pretrained=True)  # fix_me
-    torch.save(model.config, OUTPUT_DIR + 'config.pth')
-    model.to(device)
-
-    def get_optimizer_params(model, encoder_lr, decoder_lr, weight_decay=0.0):
-        param_optimizer = list(model.named_parameters())
-        no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-        optimizer_parameters = [
-            {'params': [p for n, p in model.model.named_parameters() if not any(nd in n for nd in no_decay)],
-             'lr': encoder_lr, 'weight_decay': weight_decay},
-            {'params': [p for n, p in model.model.named_parameters() if any(nd in n for nd in no_decay)],
-             'lr': encoder_lr, 'weight_decay': 0.0},
-            {'params': [p for n, p in model.named_parameters() if "model" not in n],
-             'lr': decoder_lr, 'weight_decay': 0.0}
-        ]
-        return optimizer_parameters
-
-    optimizer_parameters = get_optimizer_params(model,
-                                                encoder_lr=CONFIGURATION.encoder_lr,
-                                                decoder_lr=CONFIGURATION.decoder_lr,
-                                                weight_decay=CONFIGURATION.weight_decay)
-    optimizer = AdamW(optimizer_parameters, lr=CONFIGURATION.encoder_lr, eps=CONFIGURATION.eps, betas=CONFIGURATION.betas)
-
-    # ====================================================
-    # scheduler
-    # ====================================================
-    def get_scheduler(cfg, optimizer, num_train_steps):
-        if cfg.scheduler == 'linear':
-            scheduler = get_linear_schedule_with_warmup(
-                optimizer, num_warmup_steps=cfg.num_warmup_steps, num_training_steps=num_train_steps
-            )
-        elif cfg.scheduler == 'cosine':
-            scheduler = get_cosine_schedule_with_warmup(
-                optimizer, num_warmup_steps=cfg.num_warmup_steps, num_training_steps=num_train_steps,
-                num_cycles=cfg.num_cycles
-            )
-        return scheduler
-
-    num_train_steps = int(len(train_folds) / CONFIGURATION.batch_size * CONFIGURATION.epochs)
-    scheduler = get_scheduler(CONFIGURATION, optimizer, num_train_steps)
-
-    # ====================================================
-    # loop
-    # ====================================================
-    criterion = nn.BCEWithLogitsLoss(reduction="none")
-
-    best_score = 0.
-
-    for epoch in range(CONFIGURATION.epochs):
-
-        start_time = time.time()
-
-        # TRAIN
-        avg_loss = train_fn(fold, train_loader, model, criterion, optimizer, epoch, scheduler, device)
-
-        # eval
-        avg_val_loss, predictions = valid_fn(valid_loader, model, criterion, device)
-        predictions = predictions.reshape((len(valid_folds), CONFIGURATION.max_len))
-
-        # scoring
-        char_probs = get_char_probs(valid_texts, predictions, CONFIGURATION.tokenizer)
+if __name__ == '__main__':
+
+    def get_result(oof_df):
+        labels = create_labels_for_scoring(oof_df)
+        predictions = oof_df[[i for i in range(CONFIGURATION.max_len)]].values
+        char_probs = get_char_probs(oof_df['pn_history'].values, predictions, CONFIGURATION.tokenizer)
         results = get_results(char_probs, th=0.5)
         preds = get_predictions(results)
-        score = get_score(valid_labels, preds)
+        score = get_score(labels, preds)
+        LOGGER.info(f'Score: {score:<.4f}')
 
-        elapsed = time.time() - start_time
-
-        LOGGER.info(
-            f'Epoch {epoch + 1} - avg_train_loss: {avg_loss:.4f}  avg_val_loss: {avg_val_loss:.4f}  time: {elapsed:.0f}s')
-        LOGGER.info(f'Epoch {epoch + 1} - Score: {score:.4f}')
-        if CONFIGURATION.wandb:
-            wandb.log({f"[fold{fold}] epoch": epoch + 1,
-                       f"[fold{fold}] avg_train_loss": avg_loss,
-                       f"[fold{fold}] avg_val_loss": avg_val_loss,
-                       f"[fold{fold}] score": score})
-
-        if best_score < score:
-            best_score = score
-            LOGGER.info(f'Epoch {epoch + 1} - Save Best Score: {best_score:.4f} Model')
-            torch.save({'model': model.state_dict(),
-                        'predictions': predictions},
-                       OUTPUT_DIR + f"{CONFIGURATION.model.replace('/', '-')}_fold{fold}_best.pth")
-
-    predictions = torch.load(OUTPUT_DIR + f"{CONFIGURATION.model.replace('/', '-')}_fold{fold}_best.pth",
-                             map_location=torch.device('cpu'))['predictions']
-    valid_folds[[i for i in range(CONFIGURATION.max_len)]] = predictions
-
-    torch.cuda.empty_cache()
-    gc.collect()
-
-    return valid_folds
-
-
-if __name__ == '__main__':
 
     if CONFIGURATION.train:
         oof_df = pd.DataFrame()
         for fold in range(CONFIGURATION.n_fold):
             if fold in CONFIGURATION.trn_fold:
-                _oof_df = train_loop(train, fold)  # fix_me
+                _oof_df = train_loop(train, fold)
                 oof_df = pd.concat([oof_df, _oof_df])
                 LOGGER.info(f"========== fold: {fold} result ==========")
                 get_result(_oof_df)
@@ -320,3 +63,7 @@ if __name__ == '__main__':
 
     if CONFIGURATION.wandb:
         wandb.finish()
+
+    gc.collect()
+    torch.cuda.empty_cache()
+    wandb.agent(sweep_id, train, count=5)
